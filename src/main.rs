485c9ced3356c2e82b466c10dc9eabfcb690b917
Kaspar Emanuel <kaspar@kitspace.org>
2024-11-14 16:04:46 +0000

    Use matching for answers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use scryer_prolog::MachineBuilder;
-//use scryer_prolog::{LeafAnswer, Term};
+use scryer_prolog::LeafAnswer;
 
 fn main() {
     let mut machine = MachineBuilder::default().build();
@@ -8,14 +8,28 @@ fn main() {
 
     machine.load_module_string("logic", file_content);
 
-    let query = r#"
-        card(Id, Distance, Temp, OrbitTime, Radius, Mass, EarthSimilarity),
-        X = card(Id, Distance, Temp, OrbitTime, Radius, Mass, EarthSimilarity).
+    //let query = r#"
+    //    once(init(State)).
+    //"#;
+    //let answers = machine.run_query(query);
+
+    //for answer in answers {
+    //    println!("{:?}", answer);
+    //}
+
+    let query2 = r#"
+        init(State), random_options(State, StateOut).
     "#;
-    let answers = machine.run_query(query);
+
+    let answers = machine.run_query(query2);
 
     for answer in answers {
-        println!("{:?}", answer);
+        match answer {
+            Ok(LeafAnswer::LeafAnswer { bindings, .. }) => {
+                println!("{:?}", bindings.get("StateOut"));
+            }
+            Ok(LeafAnswer::False) => {}
+            _ => panic!("Unexpected answer: {:?}", answer),
+        }
     }
 }
-
