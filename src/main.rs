7d2fbdef46982a9cbe2438a12cbb15fb4c8b2725
Kaspar Emanuel <kaspar@kitspace.org>
2024-11-14 16:20:47 +0000

    Get a assoc t!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use scryer_prolog::MachineBuilder;
-use scryer_prolog::LeafAnswer;
+use scryer_prolog::{LeafAnswer, MachineBuilder, Term};
 
 fn main() {
     let mut machine = MachineBuilder::default().build();
@@ -8,26 +7,20 @@ fn main() {
 
     machine.load_module_string("logic", file_content);
 
-    //let query = r#"
-    //    once(init(State)).
-    //"#;
-    //let answers = machine.run_query(query);
-
-    //for answer in answers {
-    //    println!("{:?}", answer);
-    //}
-
-    let query2 = r#"
+    let query = r#"
         init(State), random_options(State, StateOut).
     "#;
 
-    let answers = machine.run_query(query2);
+    let answers = machine.run_query(query);
 
     for answer in answers {
         match answer {
-            Ok(LeafAnswer::LeafAnswer { bindings, .. }) => {
-                println!("{:?}", bindings.get("StateOut"));
-            }
+            Ok(LeafAnswer::LeafAnswer { bindings, .. }) => match bindings.get("StateOut") {
+                Some(Term::Compound(str, _args)) if str == "t" => {
+                    println!("its a t!")
+                }
+                _ => panic!("Unexpected bindings: {:?}", bindings),
+            },
             Ok(LeafAnswer::False) => {}
             _ => panic!("Unexpected answer: {:?}", answer),
         }
