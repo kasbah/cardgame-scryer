809b796f6c9bf27b162e97705d92d029c183ed13
Kaspar Emanuel <kaspar@kitspace.org>
2024-11-18 16:55:54 +0000

    Add non-recursive from_prolog_assoc

--- src/prolog_types.rs
+++ src/prolog_types.rs
@@ -4,26 +4,53 @@ use scryer_prolog::{
 };
 use std::collections::BTreeMap;
 
+pub fn from_prolog_assoc(term: &Term) -> BTreeMap<String, Term> {
+    match &term {
+        Compound(str, args) if str == "t" => match &args[..] {
+            [Atom(key), value, rest @ ..] => {
+                let mut map = BTreeMap::new();
+                map.insert(key.clone(), value.clone());
+                match rest {
+                    [Atom(s), terms @ ..] if s == "<" || s == "-" => {
+                        for term in terms {
+                            let mut m = from_prolog_assoc(term);
+                            map.append(&mut m);
+                        }
+                        map
+                    }
+                    _ => panic!("Unexpected rest args: {:?}", rest),
+                }
+            }
+            _ => panic!("Unexpected args: {:?}", args),
+        },
+        Atom(str) if str == "t" => BTreeMap::new(),
+        _ => panic!("Unexpected term: {:?}", term),
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum TermOrAssoc {
     Term(Term),
     Assoc(BTreeMap<String, TermOrAssoc>),
 }
 
-pub fn from_prolog_assoc(term: &Term) -> TermOrAssoc {
+pub fn from_prolog_assoc_recursive(term: &Term) -> TermOrAssoc {
     match &term {
         Compound(str, args) if str == "t" => match &args[..] {
             [Atom(key), value, rest @ ..] => {
                 let mut map = BTreeMap::new();
-                let v = from_prolog_assoc(value);
+                let v = from_prolog_assoc_recursive(value);
                 map.insert(key.clone(), v);
                 match rest {
                     [Atom(s), terms @ ..] if s == "<" || s == "-" => {
                         for term in terms {
-                            let mut result = from_prolog_assoc(term);
+                            let mut result = from_prolog_assoc_recursive(term);
                             match &mut result {
                                 TermOrAssoc::Assoc(m) => map.append(m),
-                                _ => panic!("Unexpected from_prolog_assoc result: {:?}", result),
+                                _ => panic!(
+                                    "Unexpected from_prolog_assoc_recursive result: {:?}",
+                                    result
+                                ),
                             }
                         }
                         TermOrAssoc::Assoc(map)
@@ -62,7 +89,7 @@ mod test {
     fn test_from_prolog_empty_assoc() {
         let term = Atom("t".to_string());
         let result = from_prolog_assoc(&term);
-        assert_eq!(result, TermOrAssoc::Assoc(BTreeMap::new()));
+        assert_eq!(result, BTreeMap::new());
     }
 
     #[test]
@@ -80,10 +107,7 @@ mod test {
         let result = from_prolog_assoc(&term);
         assert_eq!(
             result,
-            TermOrAssoc::Assoc(BTreeMap::from([(
-                "a".to_string(),
-                TermOrAssoc::Term(Atom("b".to_string()))
-            )]))
+            BTreeMap::from([("a".to_string(), Atom("b".to_string()))])
         );
     }
 
@@ -111,10 +135,10 @@ mod test {
         let result = from_prolog_assoc(&term);
         assert_eq!(
             result,
-            TermOrAssoc::Assoc(BTreeMap::from([
-                ("a".to_string(), TermOrAssoc::Term(Atom("b".to_string()))),
-                ("c".to_string(), TermOrAssoc::Term(Atom("d".to_string())))
-            ]))
+            BTreeMap::from([
+                ("a".to_string(), Atom("b".to_string())),
+                ("c".to_string(), Atom("d".to_string()))
+            ])
         );
     }
 
@@ -151,11 +175,11 @@ mod test {
         let result = from_prolog_assoc(&term);
         assert_eq!(
             result,
-            TermOrAssoc::Assoc(BTreeMap::from([
-                ("a".to_string(), TermOrAssoc::Term(Atom("b".to_string()))),
-                ("c".to_string(), TermOrAssoc::Term(Atom("d".to_string()))),
-                ("e".to_string(), TermOrAssoc::Term(Atom("f".to_string())))
-            ]))
+            BTreeMap::from([
+                ("a".to_string(), Atom("b".to_string())),
+                ("c".to_string(), Atom("d".to_string())),
+                ("e".to_string(), Atom("f".to_string()))
+            ])
         );
     }
 
@@ -174,6 +198,160 @@ mod test {
         let result = from_prolog_assoc(&term);
         assert_eq!(
             result,
+            BTreeMap::from([("a".to_string(), Term::Integer(Integer::from(1)))])
+        );
+    }
+
+    #[test]
+    fn test_from_prolog_assoc_machine() {
+        let mut machine = MachineBuilder::default().build();
+        machine.load_module_string("test", r#":- use_module(library(assoc))."#);
+
+        let query = r#"
+            list_to_assoc([a-b, c-[d(x), e(x)], f-1, g-(h-i), j-k], X).
+        "#;
+        let term = query_once_binding(&mut machine, query, "X");
+        let result = from_prolog_assoc(&term);
+        assert_eq!(
+            result,
+            BTreeMap::from([
+                ("a".to_string(), Atom("b".to_string())),
+                (
+                    "c".to_string(),
+                    Term::List(vec![
+                        Compound("d".to_string(), vec![Atom("x".to_string())]),
+                        Compound("e".to_string(), vec![Atom("x".to_string())])
+                    ])
+                ),
+                ("f".to_string(), Term::Integer(Integer::from(1))),
+                (
+                    "g".to_string(),
+                    Compound(
+                        "-".to_string(),
+                        vec![Atom("h".to_string()), Atom("i".to_string())]
+                    )
+                ),
+                ("j".to_string(), Atom("k".to_string()))
+            ])
+        );
+    }
+
+    #[test]
+    fn test_from_prolog_empty_assoc_recursive() {
+        let term = Atom("t".to_string());
+        let result = from_prolog_assoc_recursive(&term);
+        assert_eq!(result, TermOrAssoc::Assoc(BTreeMap::new()));
+    }
+
+    #[test]
+    fn test_from_prolog_assoc1_recursive() {
+        let term = Compound(
+            "t".to_string(),
+            Vec::from([
+                Atom("a".to_string()),
+                Atom("b".to_string()),
+                Atom("-".to_string()),
+                Atom("t".to_string()),
+                Atom("t".to_string()),
+            ]),
+        );
+        let result = from_prolog_assoc_recursive(&term);
+        assert_eq!(
+            result,
+            TermOrAssoc::Assoc(BTreeMap::from([(
+                "a".to_string(),
+                TermOrAssoc::Term(Atom("b".to_string()))
+            )]))
+        );
+    }
+
+    #[test]
+    fn test_from_prolog_assoc2_recursive() {
+        let term = Compound(
+            "t".to_string(),
+            Vec::from([
+                Atom("c".to_string()),
+                Atom("d".to_string()),
+                Atom("<".to_string()),
+                Compound(
+                    "t".to_string(),
+                    Vec::from([
+                        Atom("a".to_string()),
+                        Atom("b".to_string()),
+                        Atom("-".to_string()),
+                        Atom("t".to_string()),
+                        Atom("t".to_string()),
+                    ]),
+                ),
+                Atom("t".to_string()),
+            ]),
+        );
+        let result = from_prolog_assoc_recursive(&term);
+        assert_eq!(
+            result,
+            TermOrAssoc::Assoc(BTreeMap::from([
+                ("a".to_string(), TermOrAssoc::Term(Atom("b".to_string()))),
+                ("c".to_string(), TermOrAssoc::Term(Atom("d".to_string())))
+            ]))
+        );
+    }
+
+    #[test]
+    fn test_from_prolog_assoc3_recursive() {
+        let term = Compound(
+            "t".to_string(),
+            Vec::from([
+                Atom("c".to_string()),
+                Atom("d".to_string()),
+                Atom("-".to_string()),
+                Compound(
+                    "t".to_string(),
+                    Vec::from([
+                        Atom("a".to_string()),
+                        Atom("b".to_string()),
+                        Atom("-".to_string()),
+                        Atom("t".to_string()),
+                        Atom("t".to_string()),
+                    ]),
+                ),
+                Compound(
+                    "t".to_string(),
+                    Vec::from([
+                        Atom("e".to_string()),
+                        Atom("f".to_string()),
+                        Atom("-".to_string()),
+                        Atom("t".to_string()),
+                        Atom("t".to_string()),
+                    ]),
+                ),
+            ]),
+        );
+        let result = from_prolog_assoc_recursive(&term);
+        assert_eq!(
+            result,
+            TermOrAssoc::Assoc(BTreeMap::from([
+                ("a".to_string(), TermOrAssoc::Term(Atom("b".to_string()))),
+                ("c".to_string(), TermOrAssoc::Term(Atom("d".to_string()))),
+                ("e".to_string(), TermOrAssoc::Term(Atom("f".to_string())))
+            ]))
+        );
+    }
+
+    #[test]
+    fn test_from_prolog_assoc_int_recursive() {
+        let term = Compound(
+            "t".to_string(),
+            Vec::from([
+                Atom("a".to_string()),
+                Term::Integer(Integer::from(1)),
+                Atom("-".to_string()),
+                Atom("t".to_string()),
+                Atom("t".to_string()),
+            ]),
+        );
+        let result = from_prolog_assoc_recursive(&term);
+        assert_eq!(
+            result,
             TermOrAssoc::Assoc(BTreeMap::from([(
                 "a".to_string(),
                 TermOrAssoc::Term(Term::Integer(Integer::from(1)))
@@ -182,7 +360,7 @@ mod test {
     }
 
     #[test]
-    fn test_from_prolog_assoc_machine() {
+    fn test_from_prolog_assoc_machine_recursive() {
         let mut machine = MachineBuilder::default().build();
 
         machine.load_module_string("test", r#":- use_module(library(assoc))."#);
@@ -191,7 +369,7 @@ mod test {
             list_to_assoc([a-b, c-[d(x), e(x)], f-1, g-(h-i), j-k], X).
         "#;
         let term = query_once_binding(&mut machine, query, "X");
-        let result = from_prolog_assoc(&term);
+        let result = from_prolog_assoc_recursive(&term);
         assert_eq!(
             result,
             TermOrAssoc::Assoc(BTreeMap::from([
